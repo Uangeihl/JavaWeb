52abca05146d96cea85e8b57db1c5410173f8c53
huanglei <1642331764@qq.com>
2022-06-07 10:43:20 +0800

    6.7 commit

---
+++ springbootdemo1/src/main/java/com/huanglei/springbootdemo1/Springbootdemo1Application.java
@@ -0,0 +1,25 @@
+package com.huanglei.springbootdemo1;
+
+import com.huanglei.springbootdemo1.config.AuthorSettings;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class Springbootdemo1Application {
+
+    @Autowired
+    private AuthorSettings authorSettings;
+
+    @RequestMapping("/")
+    String index(){
+        return "author name is "+authorSettings.getName()+" and author age is "+authorSettings.getAge();
+    }
+    public static void main(String[] args) {
+        SpringApplication.run(Springbootdemo1Application.class, args);
+    }
+
+}
---
+++ springbootweb1/src/test/java/com/huanglei/springbootweb1/Springbootweb1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huanglei.springbootweb1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springbootweb1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
